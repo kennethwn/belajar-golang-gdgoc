98e4bed551c03a5735d20c2419a937fcae54d71e
kennethwn <kenthnov117@gmail.com>
2025-02-08 09:39:06 +0700

    chore: initial commit

--- struct/main.go
+++ struct/main.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
-type User struct {
+type UserCity struct {
 	name, city string
 }
 
+type User struct {
+	id       int
+	name     string
+	username string
+	password string
+}
+
+func (u User) getName() string {
+	return u.name
+}
+
 func main() {
-	user := User{"Kenneth", "Jakarta"}
+	/* Struct without pointer */
+	user := UserCity{"Kenneth", "Jakarta"}
 	newUser := user
 
 	user.city = "Bandung"
@@ -15,33 +27,23 @@ func main() {
 	fmt.Println(user)
 	fmt.Println(newUser)
 
-	// var user1 User
-	// user1.id = 1
-	// user1.name = "Kenneth William"
-	// user1.username = "kennethwilliam"
-	// user1.password = "backend2025"
+	/* Declare, assign, accessing struct */
+	var user1 User
+	user1.id = 1
+	user1.name = "Kenneth William"
+	user1.username = "kennethwilliam"
+	user1.password = "backend2025"
 
-	// user2 := User{
-	// 	id:       2,
-	// 	name:     "Joshua",
-	// 	username: "joshuawenata",
-	// 	password: "joshua123",
-	// }
+	user2 := User{
+		id:       2,
+		name:     "Joshua",
+		username: "joshuawenata",
+		password: "joshua123",
+	}
 
-	// // fmt.Println(user1)
-	// // fmt.Println(user2)
+	fmt.Println(user1)
+	fmt.Println(user2)
 
-	// fmt.Println(user1.getName())
-	// fmt.Println(user2.getName())
+	fmt.Println(user1.getName())
+	fmt.Println(user2.getName())
 }
-
-// type User struct {
-// 	// id       int
-// 	// name     string
-// 	// username string
-// 	// password string
-// }
-
-// func (u User) getName() string {
-// 	return u.name
-// }
